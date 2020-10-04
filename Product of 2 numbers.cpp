e4750894adafdb4a416783304fdbc31782faa274
Varun Grover <69295765+Thevarungrover@users.noreply.github.com>
2020-10-04 04:23:00 -0700

    added file to get product

---
+++ Product of 2 numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	float a,b;
+	cout<<"Enter 2 numbers to get their product : ";
+	cin>>a>>b; // READING TWO VALUES ENTERED BY USER
+	cout<<"\nProduct is : "<<(a*b); // GIVING OUTPUT
+}
